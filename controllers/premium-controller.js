07d69a1bd6844cc876c2fa44c90358a90d01e777
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-03-19 14:18:03 +0530

    Leaderboard feature DB querying optimized

--- controllers/premium-controller.js
+++ controllers/premium-controller.js
@@ -1,35 +1,22 @@
 const User = require('../models/user-model');
 const Expenses = require('../models/expenses-model');
+const sequelize = require('sequelize');
 
 exports.showLeaderboards = async (req,res,next) => {
  try{
-    const users = await User.findAll();
-    const expenses = await Expenses.findAll();
-    const userAggregatedExpenses = {};
-    //console.log("All users:", JSON.stringify(users, null, 2));
-    expenses.forEach((expense)=>{
-        if(userAggregatedExpenses[expense.userDetailId])
-        userAggregatedExpenses[expense.userId] += expense.price;
-        else userAggregatedExpenses[expense.userDetailId] = expense.price;
-    })
-    //console.log(userAggregatedExpenses);
-    const userLeaderBoardDetails = [];
-    users.forEach(user => {
-        userLeaderBoardDetails.push({name:user.name, total_cost: userAggregatedExpenses[user.id]??0})
-    })
-    userLeaderBoardDetails.sort((a,b) => b.total_cost-a.total_cost)
-    
-/*     for(let i = 0; i < userLeaderBoardDetails.length; i++){
-        let cindex = i;
-        for(let j = i+1; j < userLeaderBoardDetails.length; j++){
-            if(userLeaderBoardDetails[j].total_cost<userLeaderBoardDetails[cindex].total_cost)
-            cindex = j;
-        }
-        let temp = userLeaderBoardDetails[i];
-        userLeaderBoardDetails[i] = userLeaderBoardDetails[cindex];
-        userLeaderBoardDetails[cindex] = temp;
-    } */
-    console.log(userLeaderBoardDetails)
+    const userLeaderBoard = await User.findAll({
+        include: [
+            { 
+                model:Expenses,
+                attributes: []
+             }
+        ],
+        attributes: ['Id','name',[sequelize.fn('sum', sequelize.col('price')), 'total_cost' ]],
+        group: ['user-details.id'],
+        order: [['total_cost','DESC']]
+
+    });
+    res.status(200).json(userLeaderBoard);
 }
  catch(err) {
     console.log(err);
