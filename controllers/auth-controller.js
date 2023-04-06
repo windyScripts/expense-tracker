5a5eb6eb058c846c80b5c940d81e0d61ed55b0c6
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-04-06 14:46:09 +0530

    Added status codes and responses for authentication failure

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -72,11 +72,17 @@ exports.login = async (req,res,next) => {
 }
 
 exports.authorization = async(req,res,next) => {
+    if(req.header('Authorization')===undefined) {
+        return res.status(400).json({message: "Bad Request"})
+    }
 try{
     const token = req.header('Authorization');
     console.log(token, '12345');
     const id = Number(jwt.verify(token, '12345').userId);
     const user = await User.findByPk(id);
+    if(user === null) {
+        return res.status(401).json({message: "Unauthorized"})
+    }
     req.user = user;
     next();
 } catch(err){
