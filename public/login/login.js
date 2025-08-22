46767e96b23c5c4f2b7f1e6adccade316a5e169b
Aravind <aravindshenoy@ideaclan.com>
2025-08-22 13:40:41 +0530

    adds more adjustments for railway

--- public/login/login.js
+++ public/login/login.js
@@ -2,7 +2,7 @@
 const hostName = '3.26.180.199';
 const port = 3000;
 const domain = `${scheme}://${hostName}:${port}`; */
-const domain = 'http://localhost:3002';
+const domain = window.location.origin;
 
 const form = document.querySelector('#form');
 const emailField = document.querySelector('#email');
